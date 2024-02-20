f0f9e90a897197e0ab67ea20d41e34e6d435684c
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-02-20 22:55:47 +0530

    Update index.js

--- pages/winners/index.js
+++ pages/winners/index.js
@@ -2,7 +2,9 @@ const Winners = () => {
   return (
     <div className="bg-purple-700/30 h-full">
       <div className="flex justify-center  h-100 pt-60 text-accent text-bold text-xl w-100">
-        Winner section still under developement :)
+        We will announce our winner here
+        <br></br>
+        and past winners here :)
       </div>
     </div>
   );
