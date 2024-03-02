c7213d75cb3ae9139dba26bfcca248885973eb1f
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-03-02 13:41:18 +0530

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -7,7 +7,7 @@ import {
   HiChatBubbleBottomCenterText,
   HiEnvelope,
   HiSparkles,
-  HiChatBubbleBottomCenterText  
+  HiChatBubbleBottomCenter  
 } from "react-icons/hi2";
 
 // nav data
@@ -29,7 +29,7 @@ export const navData = [
     {
     name: "Contact",
     path: "/contact",
-    icon: <HiChatBubbleBottomCenterText  />,
+    icon: <HiChatBubbleBottomCenter  />,
   },
 ];
 
