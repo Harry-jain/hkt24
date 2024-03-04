1a1d4569649a3ca76be8d4b5737620e43d06ecf6
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-03-04 17:30:05 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Socials from "../components/Socials";
 
 const Header = () => {
   return (
-    <header className="absolute z-50 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <header className="absolute z-50 w-full flex items-center px-16 xl:px-0 xl:h-[80px]">
       <div className=" container mx-auto ">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           <Link href={"/"}>
