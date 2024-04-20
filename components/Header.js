00a6da41caf188eef94557beac813fae37d9db90
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-04-20 13:08:29 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="absolute z-50 w-full flex items-center px-16 xl:px-0 xl:h-[80px]">
       <div className=" container mx-auto ">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          <Link href={"/"}>
+          <Link href={"https://eesa24.vercel.app/"}>
             <Image
               src={"/logo.png"}
               width={40}
