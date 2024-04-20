11230c7358f7efc850eaa2c419871eaab7cb95b4
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-04-20 13:58:38 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,7 @@ const Header = () => {
     <header className="absolute z-50 w-full flex items-center px-16 xl:px-0 xl:h-[80px]">
       <div className=" container mx-auto ">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          <Link >
-            <a href="https.//eesa24.vercel.app">
+          <Link>
               <Image
                 src={"/logo.png"}
                 width={40}
@@ -16,7 +15,6 @@ const Header = () => {
                 alt=""
                 priority={true}
               />
-            </a>
           </Link>
           <Socials />
         </div>
