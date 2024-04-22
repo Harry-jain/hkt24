6868bee353818e5fe0b3143ec2b5f1a99946605e
Harry-jain <109072049+Harry-jain@users.noreply.github.com>
2024-04-22 23:24:43 +0530

    Update index.js

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,6 +2,7 @@ import { BsArrowRight } from "react-icons/bs";
 
 import { motion } from "framer-motion"; 
 import { BsGearWideConnected } from "react-icons/bs";
+
 import { fadeIn } from "../../variants";
 // components/VideoPlayer.js
 
@@ -11,8 +12,8 @@ import { fadeIn } from "../../variants";
 const Contact = () => {
   return (
     <div className="h-full bg-indigo-500/10 ">
-      <div className="xl:pr-[107px] xl:pl-[107px] pr-[35px] pl-[35px] pt-[140px]">
-        <div className="bg-purple-500/30 h-[420px] w-full rounded-lg">
+      <div className="xl:pr-[107px] xl:pl-[107px] pr-[35px] pl-[35px] pt-[140px] xl:pt-[180px] flex justify-center">
+        <div className="bg-purple-500/30 h-[300px] w-[680px] rounded-lg">
           <motion.div
             variants={fadeIn("down", 0.7)}
             initial="hidden"
@@ -20,13 +21,32 @@ const Contact = () => {
             // exit="hidden"
             className="flex-1 flex flex-col justify-center"
           >
-          <span className="text-accent flex justify-center text-2xl pt-[75px] font-black text-3xl text-center">Registeration   </span>
-          <div className="flex justify-center text-white text-2xl pb-10 pl-10 pr-10 text-center">link will be activated soon and the below given gear will be replaced by form link.</div>
+            <span className="text-accent flex justify-center text-2xl pt-[55px] font-black text-3xl text-center">&lt;Register for HACKATRONICS/&gt;</span>
+          <div className="flex justify-center text-white text-2xl pb-10 pl-10 pr-10 text-center">Register for hackatronics and read the rules, regulations and avoidence guidelines given below.</div>
           </motion.div>
 
-          <div className="flex justify-center text-sky-500 text-[90px] duration-[4200ms] font-black animate-spin-slow" viewBox="0 0 54 54">
-            <BsGearWideConnected />
+          <motion.div
+            variants={fadeIn("up",1)}
+            initial="hidden"
+            animate="show"
+            // exit="hidden"
+            className="flex-1 flex flex-col justify-center"
+          >
+            <div className="flex justify-center text-lg duration-[4200ms] font-black" viewBox="0 0 54 54">
+              <div className="sm:justify-center flex pl-12 xl:pl-0">
+                <div
+                  className="z-30  cursor-pointer justify-center flex items-center rounded-md
+                  text-white bg-accent
+                hover:border-accent hover:text-accent hover:bg-white 
+                  h-[30px] w-[320px] "
+                >
+                  <a href={"https://forms.office.com/pages/responsepage.aspx?id=fw2wx5mtKkSxL8LJEgRP3FPDWp8eQFtLsFcqoo-_-SJUOEI5RFgyOFFRTlRJQlVZNTU0VDIxNldGQS4u"}>
+                    &lt;!--Click on this tag to register--&gt;
+                  </a>
+                </div>
+              </div>
           </div>
+          </motion.div>
        </div>
       </div>
     </div>
